0930e54cded0c8c48b8b6302b8b0ac9f9f5d036d
Carlos Ruiz <ruizcarlos1985@gmail.com>
2026-02-03 11:47:28 -0300

    first commit

---
+++ SDKQAiOS/SDKQAiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SDKQAiOS
+//
+//  Created by Carlos Ruiz on 03-02-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
